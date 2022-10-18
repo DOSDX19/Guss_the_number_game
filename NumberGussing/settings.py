e181c9c155311432b1c9af838670f0222469b379
Ahmed <ahmad1dx@gmail.com>
2022-10-18 07:51:58 +0200

    77

--- NumberGussing/settings.py
+++ NumberGussing/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'NumberGussing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

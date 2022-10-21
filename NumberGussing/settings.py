6ebe21428fc98147834f8020e7eced07c76a2449
Ahmed <ahmad1dx@gmail.com>
2022-10-21 09:53:31 +0200

    'neww'

--- NumberGussing/settings.py
+++ NumberGussing/settings.py
@@ -40,7 +40,6 @@ ALLOWED_HOSTS = ['number-gussing.herokuapp.com']
 
 INSTALLED_APPS = [
     'game',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
